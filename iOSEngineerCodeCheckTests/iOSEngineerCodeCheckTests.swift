855730f536263a8275d4baac9276207ed6e66c57
SNQ-2001 <taishin05109@gmail.com>
2022-05-21 23:50:41 +0900

    簡単なテストを追加

--- iOSEngineerCodeCheckTests/iOSEngineerCodeCheckTests.swift
+++ iOSEngineerCodeCheckTests/iOSEngineerCodeCheckTests.swift
@@ -8,27 +8,51 @@
 
 import XCTest
 @testable import iOSEngineerCodeCheck
+import Alamofire
 
 class iOSEngineerCodeCheckTests: XCTestCase {
 
     let viewModel = ViewModel()
 
-    override func setUpWithError() throws {
-        // ここにセットアップコードを入れます。このメソッドは、クラス内の各テストメソッドが呼び出される前に呼び出されます。
+    var viewController: ViewController!
+
+    override func setUp() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        self.viewController = storyboard.instantiateInitialViewController() as? ViewController
     }
 
-    override func tearDownWithError() throws {
-        // ティアダウンコードをここに置く。このメソッドは、クラス内の各テストメソッドの呼び出しの後に呼び出されます。
+    override func tearDown() {
+        super.tearDown()
     }
 
-    func testDecode() {
+    // リセットが機能しているかテスト
+    func testResetPageCount() {
+        self.viewModel.resetSearchRepositories()
         
+        XCTAssertEqual(viewModel.pageCount, 1)
     }
 
-    func testPerformanceExample() throws {
-        // これはパフォーマンステストケースの例です。
+    // リセットが機能しているかテスト
+    func testResetRepo() {
+        self.viewModel.resetSearchRepositories()
+
+        XCTAssertTrue(viewModel.repo.isEmpty)
+    }
+
+    // 言語の使用割合を正しく計算できているかテスト
+    func testCreateLanguageArrays() {
+        let languagesNameArray: [String] = ["C++", "Swift", "C", "Python", "CMake", "Objective-C", "Shell", "Objective-C++", "LLVM", "Emacs Lisp", "Batchfile", "Vim script", "Roff", "DTrace", "Makefile", "Ruby", "D", "sed"]
+        let languagesValueArray: [Int] = [46780865, 38714129, 5428926, 1967517, 694021, 466082, 215387, 159688, 74481, 57594, 45943, 20025, 3683, 2593, 2361, 2132, 1107, 1050]
+        
+        let (newLanguagesNameArray, newLanguagesValueArray) = self.viewModel.createLanguageArray(languagesNameArray: languagesNameArray, languagesValueArray: languagesValueArray)
+
+        XCTAssertEqual(newLanguagesNameArray, ["C++", "Swift", "C", "Python", "CMake", "Other"])
+
+        XCTAssertEqual(newLanguagesValueArray, [49.4, 40.9, 5.7, 2.0, 0.7, 1.3])
+    }
+
+    func testPerformanceExample() {
         self.measure {
-            // ここに時間を計測したいコードを入れてください。
         }
     }
 }
